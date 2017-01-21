c4109f7bdd6666702ceea5ba03286de68d9a7c35
darksun <lujun9972@gmail.com>
2017-01-21 22:16:50 +0800

    多线程取词

--- vocabulary2anki.py
+++ vocabulary2anki.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+import hashlib
+from multiprocessing.dummy import Pool
 from dictionary.baiCiZhanDictionary import BaiCiZhanDictionary
-
-class safesub(dict):
-    def __missing__(self,key):
-        return ""
+import sys
 
 def dict2anki(d,fmt):
-    return fmt.format_map(safesub(d))
+    d = defaultdict(lambda :"",d)
+    img = d.get('img')
+    mp3 = d.get('mp3')
+    return fmt.format_map(d)
 
 def vocabulary2anki(vocabulary,fmt):
     dictionary = BaiCiZhanDictionary()
@@ -13,7 +17,9 @@ def vocabulary2anki(vocabulary,fmt):
     return dict2anki(d,fmt)
 
 if __name__ == "__main__":
-    word = "hello"
-    keys = ('{mean}','{acc}','{sentence_en}','{sentence_cn}','{img}','{mp3}')
+    words = ("hello","world")
+    keys = ('word','{mean}','{acc}','{sentence_en}','{sentence_cn}','{img}','{mp3}')
     fmt = "|".join(keys)
-    print(vocabulary2anki(word,fmt))
+    pool = Pool(10)
+    s = pool.map(lambda word:vocabulary2anki(word,fmt),words)
+    print(s)
