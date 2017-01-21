1f2389232d1a25159faa58826e7aee0d5ac08d23
darksun <lujun9972@gmail.com>
2017-01-21 14:27:20 +0800

    update

--- vocabulary2anki.py
+++ vocabulary2anki.py
@@ -1,15 +1,19 @@
 from dictionary.baiCiZhanDictionary import BaiCiZhanDictionary
 
-def dict2anki(d,keys,sep='|'):
-    vals = map(lambda key:d.get(key,""),keys)
-    return sep.join(vals)
+class safesub(dict):
+    def __missing__(self,key):
+        return ""
 
-def vocabulary2anki(vocabulary,keys,sep='|'):
+def dict2anki(d,fmt):
+    return fmt.format_map(safesub(d))
+
+def vocabulary2anki(vocabulary,fmt):
     dictionary = BaiCiZhanDictionary()
     d = dictionary.search(vocabulary)
-    return dict2anki(d,keys,sep)
+    return dict2anki(d,fmt)
 
 if __name__ == "__main__":
     word = "hello"
-    keys = ('mean','accent','sentence_en','sentence_cn','img','mp3')
-    print(vocabulary2anki(word,keys))
+    keys = ('{mean}','{acc}','{sentence_en}','{sentence_cn}','{img}','{mp3}')
+    fmt = "|".join(keys)
+    print(vocabulary2anki(word,fmt))
