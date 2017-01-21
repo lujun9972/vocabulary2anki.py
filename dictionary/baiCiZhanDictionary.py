27dad952a842df10dd74b01dce89f24f573502ea
darksun <lujun9972@gmail.com>
2017-01-21 14:19:15 +0800

    update

--- dictionary/baiCiZhanDictionary.py
+++ dictionary/baiCiZhanDictionary.py
@@ -1,6 +1,7 @@
-import json
 from urllib import request
-from dictionary import Dictionary
+import json
+import re
+from .dictionary import Dictionary
 
 class BaiCiZhanDictionary(Dictionary):
     def search(self,word):
@@ -10,9 +11,10 @@ class BaiCiZhanDictionary(Dictionary):
         explains = json.loads(explains)
         result = {}
         result["img"]= explains.get('img')
-        result["mean"] = explains.get('mean_cn')
+        means = explains.get('mean_cn')
+        result["mean"] = re.sub(r'；\s+','<br>',means)
         result["sentence_en"] = explains.get('st')
         result["sentence_cn"] = explains.get('sttr')
-        result["accent "]= explains.get('accent')
-        result["mp3 "]= "http://baicizhan.qiniucdn.com/word_audios/{}.mp3".format(word)
+        result["accent"]= explains.get('accent')
+        result["mp3"]= "http://baicizhan.qiniucdn.com/word_audios/{}.mp3".format(word)
         return result
--- vocabulary2anki.py
+++ vocabulary2anki.py
@@ -1,5 +1,15 @@
-import dictionary.baiCiZhanDictionary
+from dictionary.baiCiZhanDictionary import BaiCiZhanDictionary
 
-def output_dict(d,keys,sep='|'):
-    vals = map(lambda key:d.get(key),keys)
+def dict2anki(d,keys,sep='|'):
+    vals = map(lambda key:d.get(key,""),keys)
     return sep.join(vals)
+
+def vocabulary2anki(vocabulary,keys,sep='|'):
+    dictionary = BaiCiZhanDictionary()
+    d = dictionary.search(vocabulary)
+    return dict2anki(d,keys,sep)
+
+if __name__ == "__main__":
+    word = "hello"
+    keys = ('mean','accent','sentence_en','sentence_cn','img','mp3')
+    print(vocabulary2anki(word,keys))
