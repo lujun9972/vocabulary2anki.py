e740c8c98c917135b0656e9e2854c5a6aae7bc1b
darksun <lujun9972@gmail.com>
2017-01-23 12:53:28 +0800

    update

--- vocabulary2anki.py
+++ vocabulary2anki.py
@@ -8,6 +8,7 @@ import sys
 import argparse
 from urllib import request
 from dictionary.jsonDictionary import JsonDictionary
+import fileinput
 
 def download_for_anki(url):
     if not url:
@@ -19,6 +20,10 @@ def download_for_anki(url):
         if not os.path.exists("collection.media"):
             os.makedirs("collection.media")
         request.urlretrieve(url,filepath)
+        if fileext in ('jpg','jpeg','gif','png','svg'):
+            filename =  '<img src="{}"></img>'.format(filename)
+        elif fileext in ('mp3','mp4','wav'):
+            filename = '[sound:{}]'.format(filename)
     except Exception as e:
         print("fetch {} error:{}".format(url,e),file=sys.stderr)
         filename = ""
@@ -26,10 +31,10 @@ def download_for_anki(url):
 
 def dict2anki(d,fmt):
     d = defaultdict(lambda :"",d)
-    img = download_for_anki(d.get('img'))
-    d['img'] = img and '<img src="{}"></img>'.format(img)
-    mp3 = download_for_anki(d.get('mp3'))
-    d['mp3'] = mp3 and '[sound:{}]'.format(mp3)
+    d['img'] = download_for_anki(d.get('img'))
+    # d['img'] = img and '<img src="{}"></img>'.format(img)
+    d['mp3'] = download_for_anki(d.get('mp3'))
+    # d['mp3'] = mp3 and '[sound:{}]'.format(mp3)
     return fmt.format_map(d)
 
 def vocabulary2anki(vocabulary,fmt):
@@ -40,7 +45,7 @@ def vocabulary2anki(vocabulary,fmt):
 if __name__ == "__main__":
     # 解析参数
     parser = argparse.ArgumentParser(description='查询单词意义,并以anki可以导入的方式输出')
-    parser.add_argument(dest='source_file',metavar='source_file',help='存放单词的源文件,每行一个词,如果不填,则会从标准输入读取单词')
+    parser.add_argument(dest='source_file',metavar='source_file',nargs='*',help='存放单词的源文件,每行一个词,如果不填,则会从标准输入读取单词')
     parser.add_argument('-o',dest='dest_file',metavar='dest_file',action='store',help='存放结果的目标文件,如果省略,则会写入到标准输出中')
     parser.add_argument('--fmt',dest='fmt',action='store',metavar='format',help='指定输出的格式,其中{word},{mean},{accent},{sentence_en},{sentence_cn},{img},{mp3}会被替换,默认值为"{word}|{mean}|{accent}|{sentence_en}|{sentence_cn}|{img}|{mp3}"')
     args = parser.parse_args()
@@ -48,7 +53,7 @@ if __name__ == "__main__":
     if len(args.source_file) == 0:
         source_file = sys.stdin
     else:
-        source_file = open(args.source_file,'r')
+        source_file = fileinput.input(args.source_file)
 
     if args.dest_file:
         dest_file = open(args.dest_file,'w')
@@ -61,7 +66,9 @@ if __name__ == "__main__":
         keys = ('{word}','{mean}','{accent}','{sentence_en}','{sentence_cn}','{img}','{mp3}')
         fmt = "|".join(keys)
 
-    words = source_file.readlines()
+    words = []
+    for line in source_file:
+        words.append(line)
     words = map(lambda word:word.strip(),words)
     pool = Pool(10)
     records = pool.map(lambda word:vocabulary2anki(word,fmt),words)
