8572b5f3a38f2f8688a8b37f0d3eb9328cb160f8
darksun <lujun9972@gmail.com>
2017-02-03 16:11:23 +0800

    update

--- ankiConnectClient.py
+++ ankiConnectClient.py
@@ -1,34 +1,39 @@
 import json
 from urllib import request,parse
 
-url = "http://127.0.0.1:8765"
+class AnkiConnectClient:
+    def __init__(self,url="http://127.0.0.1:8765"):
+        self.url = url
+    def _request(self,action,params):
+        "Commuicate with AnkiConnect. PARAMS should be a dict"
+        data = {"action":action,"params":params}
+        querystring = json.dumps(data)
+        resp = request.urlopen(self.url,querystring.encode('ascii'))
+        return json.loads(resp.read())
+    def getDecks(self):
+        "list decks"
+        return self._request("deckNames",{})
+    def getModels(self):
+        "list models"
+        return self._request("modelNames",{})
+    def getModelFields(self,model):
+        "list fields in MODEL"
+        return self._request("modelFieldNames",{"modelName":model})
+    def addNote(self,deck,model,fields):
+        '''add a note to DECK
+        MODEL specify the format of the note.
+        FIELD-ALIST specify the content of the note. '''
+        return self._request("addNote",{"note":{"deckName":deck,
+                                                "modelName":model,
+                                                "fields":fields,
+                                                "tags":[]}})
 
-def _request(action,params):
-    "Commuicate with AnkiConnect. PARAMS should be a dict"
-    data = {"action":action,"params":params}
-    querystring = json.dumps(data)
-    resp = request.urlopen(url,querystring.encode('ascii'))
-    return json.loads(resp.read())
-
-def getDecks():
-    "list decks"
-    return _request("deckNames",{})
-
-def getModels():
-    "list models"
-    return _request("modelNames",{})
-
-def getModelFields(model):
-    "list fields in MODEL"
-    return _request("modelFieldNames",{"modelName":model})
-
-def addNote(deck,model,fields):
-    '''add a note to DECK
-
-MODEL specify the format of the note.
-FIELD-ALIST specify the content of the note. '''
-
-    return _request("addNote",{"note":{"deckName":deck,
-                                       "modelName":model,
-                                       "fields":fields,
-                                       "tags":[]}})
+if __name__ == "__main__":
+    anki = AnkiConnectClient()
+    decks = anki.getDecks()
+    print(decks)
+    models = anki.getModels()
+    print(models)
+    model = models[-1]
+    fields = anki.getModelFields(model)
+    print(fields)
